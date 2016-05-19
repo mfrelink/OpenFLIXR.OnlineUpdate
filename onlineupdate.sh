e3d3c124f6202c311b22169806826c6f5d2e715d
mfrelink <mfrelink@me.com>
2016-05-19 20:33:09 +0200

    verify

--- onlineupdate.sh
+++ onlineupdate.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
-exec 1> >(tee -a /var/log/onlineupdate.log) 2>&1
-TODAY=$(date)
-echo "-----------------------------------------------------"
-echo "Date:          $TODAY"
-echo "-----------------------------------------------------"
-
 THISUSER=$(whoami)
     if [ $THISUSER != 'root' ]
         then
             echo 'You must use sudo to run this script, sorry!'
            exit 1
     fi
+
+#!/bin/bash
+exec 1> >(tee -a /var/log/onlineupdate.log) 2>&1
+TODAY=$(date)
+echo "-----------------------------------------------------"
+echo "Date:          $TODAY"
+echo "-----------------------------------------------------"
 
 ## Openflixr Online Update version 1.0
 echo "OpenFLIXR Online Update:"
